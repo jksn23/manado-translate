2778c6cc126c9cf9a362ca584f4fd0a9b454a31a
Jeksen Lilang <71911158+jksn23@users.noreply.github.com>
2023-07-11 11:00:41 +0800

    ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,14 @@ export default function RootLayout({ children }) {
       <head />
       <body>{children}
       <a href="https://publishers.adsterra.com/referral/YCALcCZGtM"><img alt="banner" src="https://landings-cdn.adsterratech.com/referralBanners/png/80%20x%2030%20px.png" /></a>
+      atOptions = {
+		'key' : '18f90aa1a51e5de0d0176e5a3cc1d7a3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://distressedsoultabloid.com/18f90aa1a51e5de0d0176e5a3cc1d7a3/invoke.js"></scr' + 'ipt>');
       </body>
     </html>
   )
