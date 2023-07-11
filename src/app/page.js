fbbeef7ba83430d7320cf223cc9df2fe19dadb9a
Jeksen Lilang <71911158+jksn23@users.noreply.github.com>
2023-07-11 11:05:49 +0800

    ads

--- src/app/page.js
+++ src/app/page.js
@@ -81,6 +81,16 @@ export default function Home() {
           </p>
         </a>
       </div>
+      
     </main>
+    
   )
+  atOptions = {
+		'key' : '18f90aa1a51e5de0d0176e5a3cc1d7a3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://distressedsoultabloid.com/18f90aa1a51e5de0d0176e5a3cc1d7a3/invoke.js"></scr' + 'ipt>');
 }
