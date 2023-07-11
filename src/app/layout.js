893ad3da2e563bb3f08c22cb4eb9eea761a7706f
Jeksen Lilang <71911158+jksn23@users.noreply.github.com>
2023-07-11 11:10:16 +0800

    ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
       <head />
       <body>{children}
       <a href="https://publishers.adsterra.com/referral/YCALcCZGtM"><img alt="banner" src="https://landings-cdn.adsterratech.com/referralBanners/png/80%20x%2030%20px.png" /></a>
+      
       </body>
     </html>
   )
