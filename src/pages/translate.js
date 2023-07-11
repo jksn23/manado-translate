4a3a97003ecf0714616dba6ca631d9e58aacafda
Jeksen Lilang <71911158+jksn23@users.noreply.github.com>
2023-07-11 11:40:33 +0800

    ads

--- src/pages/translate.js
+++ src/pages/translate.js
@@ -136,32 +136,10 @@ function HomePage() {
         </div>
       </form>
       <div>
-      <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              atOptions = {
-                'key' : '18f90aa1a51e5de0d0176e5a3cc1d7a3',
-                'format' : 'iframe',
-                'height' : 90,
-                'width' : 728,
-                'params' : {}
-              };
-              document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://distressedsoultabloid.com/18f90aa1a51e5de0d0176e5a3cc1d7a3/invoke.js"></scr' + 'ipt>');
-            `,
-          }}
-        />
+    
       </div>
     </div>
   );
-  atOptions = {
-		'key' : '18f90aa1a51e5de0d0176e5a3cc1d7a3',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/18f90aa1a51e5de0d0176e5a3cc1d7a3/invoke.js"></scr' + 'ipt>');
 
 }
 
