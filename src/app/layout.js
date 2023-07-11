b37acefedf71298d78585c1781fb251e2e8c8b61
Jeksen Lilang <71911158+jksn23@users.noreply.github.com>
2023-07-11 10:55:15 +0800

    ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,9 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>{children}
+      <a href="https://publishers.adsterra.com/referral/YCALcCZGtM"><img alt="banner" src="https://landings-cdn.adsterratech.com/referralBanners/png/80%20x%2030%20px.png" /></a>
+      </body>
     </html>
   )
 }
