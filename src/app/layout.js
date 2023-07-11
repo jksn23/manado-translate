085170c9e6ea1ca757029c8772f92bf9cdbad07c
Jeksen Lilang <71911158+jksn23@users.noreply.github.com>
2023-07-11 11:18:03 +0800

    ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,22 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+  <script
+    type="text/javascript"
+    dangerouslySetInnerHTML={{
+      __html: `
+        var atOptions = {
+          'key': '18f90aa1a51e5de0d0176e5a3cc1d7a3',
+          'format': 'iframe',
+          'height': 90,
+          'width': 728,
+          'params': {}
+        };
+        document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://distressedsoultabloid.com/18f90aa1a51e5de0d0176e5a3cc1d7a3/invoke.js"></scr' + 'ipt>');
+      `
+    }}
+  />
+
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
